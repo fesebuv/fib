7d196622048476adec18a1fab7fa0ce285205c4d
Felipe Buenaño <fbuenano>
2017-07-24 14:12:28 -0400

    updates
    udpates

--- src/fib.js
+++ src/fib.js
@@ -1,38 +1,19 @@
 
-
 /**
- * sum - method that controls flow and recursion.
+ * fib - returns fibonacci sequence.
  *
- * @param  {Number} num1 - number to add
- * @param  {Number} num2 - number to add
- * @param  {Number} nth  - the nth number
- * @param  {Number} aggr - aggregator
- * @return {Number} - fib number.
- */
-function sum(num1, num2, nth, aggr) {
-  var num1 = num1 || 0;
-  var num2 = num2 || 1;
-  var value = num1 + num2;
-  var aggr = aggr || 1;
-  aggr++;
-  if(aggr < nth) {
-    return sum(num2, value, nth, aggr);
-  }
-  return value;
-}
-
-/**
- * fib - fibonaci sequence
- *
- * @param  {type} nth - number
- * @return {type}     fibonaci number
+ * @param  {Number} nth - the nth number on the sequence.
+ * @return {Array}  array   - returns an array containing the sequence to the nth number.
  */
 function fib(nth) {
-  var nth = nth || 0;
+  var arr = [];
   if(nth === 0) {
-    return 0;
+    arr.push(1);
   }
-  return sum(null, null, nth, 1);
+  for (var i=0; i<nth; i++) {
+    arr.push(i < 2 ? 1 : arr[i - 1] + arr[i - 2]);
+  }
+  return arr;
 }
 
 module.exports = {
